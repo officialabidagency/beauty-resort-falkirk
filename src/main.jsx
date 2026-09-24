b81a16cc8e3119665f3bccb10b685ce887a2adef
officialabidagency <official.abidagency@gmail.com>
2026-09-23 21:49:58 -0700

    check

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,34 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+class AppErrorBoundary extends React.Component {
+  state = { hasError: false }
+
+  static getDerivedStateFromError() {
+    return { hasError: true }
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="app-error">
+          <h1>Beauty Resort Falkirk</h1>
+          <p>We are refreshing the experience. Please reload this page.</p>
+          <button type="button" onClick={() => window.location.reload()}>Reload</button>
+        </div>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AppErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppErrorBoundary>
   </React.StrictMode>,
 )
